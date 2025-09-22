498ce84a2c0c5a356a2fb6cd7ca8d51cf207f258
Yaroslava <lanhraf.yaroslava@gmail.com>
2025-09-22 22:14:38 +0300

    Index Name and Email

--- task.sql
+++ task.sql
@@ -1,7 +1,3 @@
-CREATE INDEX idx_email ON Customers (Email);
+CREATE INDEX EMAIL ON Customers (Email);
 
-CREATE INDEX idx_name ON Products (Name);
-
-CREATE INDEX idx_customerid ON Orders (CustomerID);
-
-CREATE INDEX idx_date ON Orders (Date);
+CREATE INDEX NAME ON Products (Name);
